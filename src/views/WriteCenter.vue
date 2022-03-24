f9d5672f4b9b7e6b30ed1f96619aedb31019879e
lovbe0210 <lovbe0210@sina.com>
2022-03-24 22:06:34 +0800

    写作中心微调

--- src/views/WriteCenter.vue
+++ src/views/WriteCenter.vue
@@ -10,17 +10,27 @@
       <b-tooltip target="menu_tips" :show.sync="display" placement="bottomleft" triggers="click" delay="200"
                  variant="secondary" custom-class="tips-content">
           <b-list-group class="tip-content" flush>
-            <b-list-group-item to="/">
-              <span class="iconfont icon-home"></span>首页
+            <b-list-group-item to="/" style="display: flex;align-items: center;">
+              <span class="iconfont icon-home" style="margin-right: 10px;font-size: 18px;"></span>
+              <span>首页</span>
             </b-list-group-item>
-            <b-list-group-item to="some-link">
-              <span class="iconfont icon-quick-start">快捷入口</span>
+            <b-list-group-item style="display: flex;align-items: center;">
+              <span class="iconfont icon-quick-start" style="margin-right: 10px;font-size: 18px;"></span>
+              <Dropdown trigger="hover" :transfer="false" @on-click="sortBy" placement="right-start" transfer-class-name="quick-start">
+                <span>快捷入口<span class="iconfont icon-more" style="margin-left: 40px;"></span></span>
+                <DropdownMenu slot="list">
+                  <DropdownItem v-for="item in quickStart" :key="item.uid" :name="item.title">{{item.title}}
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
             </b-list-group-item>
-            <b-list-group-item to="some-link">
-              <span class="iconfont icon-like">收藏</span>
+            <b-list-group-item style="display: flex;align-items: center;">
+              <span class="iconfont icon-like" style="margin-right: 10px;font-size: 18px;"></span>
+              <span>收藏<span class="iconfont icon-more" style="margin-left: 68px;"></span></span>
             </b-list-group-item>
-            <b-list-group-item to="some-link">
-              <span class="iconfont icon-new-doc">新建</span>
+            <b-list-group-item to="some-link" style="display: flex;align-items: center;">
+              <span class="iconfont icon-new-doc" style="margin-right: 10px;font-size: 18px;"></span>
+              <span>新建</span>
             </b-list-group-item>
           </b-list-group>
 
@@ -41,34 +51,25 @@
     name: 'Follow',
     data() {
       return {
-        startMenuList: [
+        quickStart: [
           {
             uid: "sd12",
-            value: ""
+            title: "小红书",
+            type: "1"
           },
           {
             uid: "sd12",
-            value: "首页首页首页"
+            title: "如何牢记Linux指令",
+            type: "2"
           },
           {
             uid: "asfdsf44",
-            value: "专题首页首页"
-          },
-          {
-            uid: "sd1dfsf432",
-            value: "最近首页首页"
-          },
-          {
-            uid: "sd1213ds2",
-            value: "收藏首页首页"
-          },
-          {
-            uid: "sd1678ds2",
-            value: "导入首页首页"
+            title: "JVM调优指南",
+            type: "2"
           }
         ],
         display: false,
-        title: 'asdada'
+        quickDisplay: true
       }
     },
     methods: {},
@@ -83,6 +84,10 @@
         }
       })
       this.$root.$on('bv::tooltip::hide', bvEvent => {
+        if (this.quickDisplay) {
+          bvEvent.preventDefault();
+        }
+
         if (bvEvent.target.id === 'menu_tips') {
           // 切换图标显示
           this.display = true
