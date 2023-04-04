2ebc4da641bda78b2d4d515c4e466a1d489d8e46
lovbe0210 <lovbe0210@163.com>
2023-04-04 16:49:14 +0800

    路由刷新

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '../views/Index.vue'
 import Recommend from '@/components/Recommend'
-import Login from '@/components/Login'
 
 // 解决router.push报重复路由错误，实际并没有重复
 const originalPush = VueRouter.prototype.push
@@ -14,27 +13,30 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    // 首页
     path: '/',
     name: 'Index',
     component: Index,
     redirect: {
       name: 'Recommend',
-      params: { index: 'true' }
+      params: {index: 'true'}
     },
-    // redirect: '/recommend',
     children: [
       {
+        // 关注
         path: 'follow',
         name: 'Follow',
         // 此方式为路由懒加载 component: () => import('../views/About.vue')
         component: () => import('@/components/Follow')
       },
       {
+        // 推荐
         path: 'recommend',
         name: 'Recommend',
         component: Recommend
       },
       {
+        // 主题
         path: 'topic',
         name: 'Topic',
         component: () => import('@/components/Topic')
@@ -42,11 +44,13 @@ const routes = [
     ]
   },
   {
+    // 登陆
     path: '/login',
     name: 'Login',
-    component: Login
+    component: () => import('@/components/Login')
   },
   {
+    // 写作中心
     path: '/writeCenter',
     name: 'WriteCenter',
     component: () => import('@/views/WriteCenter')
@@ -57,6 +61,14 @@ const router = new VueRouter({
   mode: 'hash',
   base: process.env.BASE_URL,
   routes
+})
+
+router.afterEach((to, from) => {
+  console.dir(from)
+  console.dir(to)
+  if (from.name === 'WriteCenter') {
+    window.location.reload();
+  }
 })
 
 export default router
