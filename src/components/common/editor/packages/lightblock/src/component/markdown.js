1bb14ab9e6396978e7a3950e2e91264f7ebeec6c
lovbe0210 <lovbe0210@163.com>
2023-08-03 21:07:38 +0800

    加入高亮块插件

---
+++ src/components/common/editor/packages/lightblock/src/component/markdown.js
@@ -0,0 +1,22 @@
+import container from "markdown-it-container"
+import { encodeCardValue } from "@aomao/engine"
+
+export default function mk_lightblock(md) {
+  const defaultValue = {
+    borderColor: "#fed4a4",
+    backgroundColor: "#fff5eb",
+    text: "light-block"
+  }
+
+  md.use(container, "tip", {
+    render(tokens, idx) {
+      if (tokens[idx].nesting === 1) {
+        return `<div data-type="lightblock" data-value="${encodeCardValue(
+          defaultValue
+        )}">`
+      } else {
+        return "</div>"
+      }
+    }
+  })
+}
---
+++ src/components/common/editor/packages/lightblock/src/index.js
@@ -0,0 +1,124 @@
+import {
+  $,
+  Plugin,
+  CARD_KEY,
+  isEngine,
+  decodeCardValue,
+  encodeCardValue,
+  READY_CARD_KEY,
+  Parser
+} from "@aomao/engine"
+import locales from "./locale"
+import LightblockComponent from "./component"
+import lightblockMk from "./component/markdown"
+
+export default class extends Plugin {
+  static get pluginName() {
+    return "lightblock"
+  }
+  init() {
+    const editor = this.editor
+
+    editor.language.add(locales)
+    editor.on("parse:html", this.parseHtml)
+    editor.on("paste:schema", this.pasteSchema)
+    editor.on("paste:each", this.pasteHtml)
+    if (isEngine(editor)) {
+      editor.on("markdown-it", this.markdownIt)
+    }
+  }
+
+  execute() {
+    const editor = this.editor
+
+    if (!isEngine(editor) || editor.readonly) return
+    const { card } = editor
+
+    card.insert(
+      LightblockComponent.cardName,
+      {
+        borderColor: "#fed4a4",
+        backgroundColor: "#fff5eb",
+        text: "light-block"
+      },
+      true
+    )
+  }
+
+  markdownIt = markdown => {
+    if (this.options.markdown !== false) {
+      lightblockMk(markdown)
+    }
+  }
+
+  pasteSchema = schema => {
+    schema.add({
+      type: "block",
+      name: "div",
+      attributes: {
+        "data-type": {
+          required: true,
+          value: LightblockComponent.cardName
+        },
+        "data-value": "*"
+      }
+    })
+  }
+
+  pasteHtml = node => {
+    const editor = this.editor
+    const cardName = LightblockComponent.cardName
+
+    if (!isEngine(editor) || editor.readonly) return
+    if (node.isElement()) {
+      const type = node.attributes("data-type")
+      if (type === cardName) {
+        const value = node.attributes("data-value")
+        const cardValue = decodeCardValue(value)
+        editor.card.replaceNode(node, cardName, cardValue)
+        node.remove()
+        return false
+      }
+    }
+    return true
+  }
+
+  parseHtml = root => {
+    const cardName = LightblockComponent.cardName
+
+    root
+      .find(`[${CARD_KEY}="${cardName}"],[${READY_CARD_KEY}="${cardName}"]`)
+      .each(cardNode => {
+        const node = $(cardNode)
+        const card = this.editor.card.find(node)
+        const value = card?.getValue()
+        if (value) {
+          node.empty()
+          const div = this.renderHtml(value, cardName)
+          node.replaceWith(div)
+        } else node.remove()
+      })
+  }
+
+  renderHtml = (value, cardName) => {
+    const htmlstring = new Parser(
+      value.html || value.text,
+      this.editor
+    ).toHTML()
+
+    return $(
+      `<div data-type="${cardName}" data-value="${encodeCardValue(
+        value
+      )}">${htmlstring}</div>`
+    )
+  }
+
+  destroy() {
+    const editor = this.editor
+
+    editor.off("parse:html", this.parseHtml)
+    editor.off("paste:schema", this.pasteSchema)
+    editor.off("paste:each", this.pasteHtml)
+  }
+}
+export { LightblockComponent }
