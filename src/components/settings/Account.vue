e339c53ef9ab6d362741da68c5a01315691d1673
lovbe0210 <lovbe0210@sina.com>
2023-08-08 22:17:17 +0800

    账户设置开发1

--- src/components/settings/Account.vue
+++ src/components/settings/Account.vue
@@ -1,6 +1,87 @@
 <template>
   <div class="layout-module_account">
-    account
+    <div id="main-right-content" class="layout-module_MainContent">
+      <div class="layout-module_MainContentChildren">
+        <div class="main-container account-setting">
+          <h1 class="setting-title">账户管理</h1>
+          <h2 class="setting-subtitle">账户绑定</h2>
+          <div class="settings-form">
+            <div class="mobile-setting-form form-item">
+              <span class="iconfont icon-account-completed"></span>
+              <div class="form-content">
+                <p>手机号码</p>
+                <span class="tip">153******57</span>
+              </div>
+              <button type="button" class="control-button">
+                <span>更改</span>
+              </button>
+            </div>
+            <div class="email-setting-form form-item">
+              <div class="empty-email form-split">
+                <svg width="1em" height="1em" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg"
+                     class="larkui-icon larkui-icon-exclamationcirclefilled setting-warning-form-icon">
+                  <path
+                    d="M128 16c61.856 0 112 50.144 112 112s-50.144 112-112 112S16 189.856 16 128 66.144 16 128 16Zm0 154c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12Zm0-100c-5.523 0-10 4.477-10 10v64c0 5.523 4.477 10 10 10s10-4.477 10-10V80c0-5.523-4.477-10-10-10Z"
+                    fill="currentColor" fill-rule="nonzero"></path>
+                </svg>
+                <div class="form-content"><p>邮箱</p>
+                  <p class="tip">未绑定，绑定后当你手机号不可用时，可通过邮箱验证更换手机号</p></div>
+              </div>
+              <button type="button" class="ant-btn ant-btn-default ant-btn-sm control-button"
+                      ant-click-animating-without-extra-node="false"><span>绑定</span></button>
+            </div>
+            <div class="password-setting-form form-item">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"
+                   class="larkui-icon larkui-icon-icon-introductioncheck setting-check-form-icon">
+                <g stroke-width="IconIntroductioncheck-1" fill="none" fill-rule="evenodd">
+                  <path d="M0 0h16v16H0z"></path>
+                  <path
+                    d="M3.111 7.899c.474-.532.944-.624 1.412-.279.613.453 1.493 1.667 2.64 3.64C9.61 6.726 10.97 4.276 11.241 3.91c.215-.292.701-.428 1.458-.407a.4.4 0 0 1 .34.59c-2.612 4.867-4.13 7.586-4.556 8.16-.66.892-2.143 1.049-2.802.617-.408-.266-1.3-1.529-2.677-3.787h-.001a1 1 0 0 1 .107-1.184Z"
+                    fill="currentColor"></path>
+                </g>
+              </svg>
+              <div class="form-content"><p>账户密码</p>
+                <p class="tip">已设置，可通过账户密码登录</p></div>
+              <button type="button" class="ant-btn ant-btn-default ant-btn-sm control-button"
+                      ant-click-animating-without-extra-node="false"><span>更改</span></button>
+            </div>
+            <div class="domain-setting-form form-item">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"
+                   class="larkui-icon larkui-icon-icon-introductioncheck setting-check-form-icon">
+                <g stroke-width="IconIntroductioncheck-1" fill="none" fill-rule="evenodd">
+                  <path d="M0 0h16v16H0z"></path>
+                  <path
+                    d="M3.111 7.899c.474-.532.944-.624 1.412-.279.613.453 1.493 1.667 2.64 3.64C9.61 6.726 10.97 4.276 11.241 3.91c.215-.292.701-.428 1.458-.407a.4.4 0 0 1 .34.59c-2.612 4.867-4.13 7.586-4.556 8.16-.66.892-2.143 1.049-2.802.617-.408-.266-1.3-1.529-2.677-3.787h-.001a1 1 0 0 1 .107-1.184Z"
+                    fill="currentColor"></path>
+                </g>
+              </svg>
+              <div class="form-content"><p>个人路径</p>
+                <p class="tip">https://www.yuque.com/lovbe0210</p>
+                <button type="button" class="ant-btn ant-btn-default ant-btn-sm control-button"
+                        ant-click-animating-without-extra-node="false"><span>更改</span></button>
+              </div>
+            </div>
+            <div class="thirdAccount-setting-form">
+              <h2 class="index-module_inherit_TQ-oW setting-subtitle" style="margin: 0px 0px 6px;">
+                绑定第三方账户
+              </h2>
+              <span class="ThirdAccount-module_tip_BBol+">绑定后通过第三方应用快速扫码登录</span>
+              <div class="ThirdAccount-module_thirdAccountList_tbw-B third-binding">
+                <div class="ThirdAccount-module_thirdAccountListItem_cWNIP"><span
+                  class="ThirdAccount-module_bindText_XibFA"><span class="icon dingtalk"></span>钉钉</span><a
+                  class="ThirdAccount-module_bindButton_9Qu2v">绑定</a></div>
+                <div class="ThirdAccount-module_thirdAccountListItem_cWNIP"><span
+                  class="ThirdAccount-module_bindText_XibFA"><span class="icon alipay"></span>支付宝</span><a
+                  class="ThirdAccount-module_bindButton_9Qu2v">绑定</a></div>
+                <div class="ThirdAccount-module_thirdAccountListItem_cWNIP"><span
+                  class="ThirdAccount-module_bindText_XibFA"><span class="icon wechat"></span>@福</span><a
+                  class="ThirdAccount-module_bindButton_9Qu2v">解绑</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -38,5 +119,5 @@
 </script>
 
 <style scoped lang="less">
-  /*@import 'src/components/css/setting/profile.less';*/
+  @import '../css/setting/account.less';
 </style>
--- src/components/settings/Profile.vue
+++ src/components/settings/Profile.vue
@@ -1,62 +1,63 @@
 <template>
   <div class="layout-module_account">
-    <h1 class="setting-title">个人信息</h1>
-    <div class="settings-form form">
-      <form class="setting-legacy-form">
-        <div class="setting-legacy-form-item legacy-form-item-avatar">
-          <div class="setting-form-item-label">
-            <label title="">
-              <span>头像</span>
-            </label>
-          </div>
-          <div class="setting-legacy-form-item-control-wrapper">
-            <div class="avatar-uploader-control">
-              <img loading="lazy" data-testid="img-avatar"
-                   :src="userInfo.avatar"
-                   class="avatar-uploader-show" alt="">
-              <div class="avatar-uploader-btn-wrapper">
-                <Upload action="/"
-                        type="drag"
-                        :format="['jpg','jpeg','png']"
-                        :max-size="5120"
-                        accept="image/*"
-                        :show-upload-list="false"
-                        :before-upload="fileHandle">
-                  <div class="avatar-uploader-btn" ref="avatarBtn">
-                    <span class="iconfont icon-avatar-upload"></span>
-                    <span>更新头像</span>
-                  </div>
-                </Upload>
-                <p class="avatar-uploader-tip">可以拖动图片到按钮区域完成上传</p>
-              </div>
-              <Modal
-                v-model="showAvatarCropper"
-                title="编辑头像"
-                :mask-closable="false"
-                @on-ok="confirmAvatarCrop">
-                <div class="avatar-cropper">
-                  <vueCropper
-                    class="crop-box"
-                    ref="cropper"
-                    :img="avatarOriginalFile"
-                    :autoCrop="true"
-                    :fixedBox="true"
-                    :centerBox="true"
-                    :fixed="true"
-                    :canScale="false"
-                    :maxImgSize="400"
-                    :info="false"
-                    @cropMoving="cropMoving"
-                    @imgLoad="imgLoadOver">
-                  </vueCropper>
-                  <div class="avatar-preview">
-                    <img loading="lazy" :src="avatarPreview" class="img" alt="预览">
-                    <div class="alt">
-                      <span>预览</span>
+    <div class="main-container">
+      <h1 class="setting-title">个人信息</h1>
+      <div class="settings-form form">
+        <form class="setting-legacy-form">
+          <div class="setting-legacy-form-item legacy-form-item-avatar">
+            <div class="setting-form-item-label">
+              <label title="">
+                <span>头像</span>
+              </label>
+            </div>
+            <div class="setting-legacy-form-item-control-wrapper">
+              <div class="avatar-uploader-control">
+                <img loading="lazy" data-testid="img-avatar"
+                     :src="userInfo.avatar"
+                     class="avatar-uploader-show" alt="">
+                <div class="avatar-uploader-btn-wrapper">
+                  <Upload action="/"
+                          type="drag"
+                          :format="['jpg','jpeg','png']"
+                          :max-size="5120"
+                          accept="image/*"
+                          :show-upload-list="false"
+                          :before-upload="fileHandle">
+                    <div class="avatar-uploader-btn" ref="avatarBtn">
+                      <span class="iconfont icon-avatar-upload"></span>
+                      <span>更新头像</span>
+                    </div>
+                  </Upload>
+                  <p class="avatar-uploader-tip">可以拖动图片到按钮区域完成上传</p>
+                </div>
+                <Modal
+                  v-model="showAvatarCropper"
+                  title="编辑头像"
+                  :mask-closable="false"
+                  @on-ok="confirmAvatarCrop">
+                  <div class="avatar-cropper">
+                    <vueCropper
+                      class="crop-box"
+                      ref="cropper"
+                      :img="avatarOriginalFile"
+                      :autoCrop="true"
+                      :fixedBox="true"
+                      :centerBox="true"
+                      :fixed="true"
+                      :canScale="false"
+                      :maxImgSize="400"
+                      :info="false"
+                      @cropMoving="cropMoving"
+                      @imgLoad="imgLoadOver">
+                    </vueCropper>
+                    <div class="avatar-preview">
+                      <img loading="lazy" :src="avatarPreview" class="img" alt="预览">
+                      <div class="alt">
+                        <span>预览</span>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div class="image-cropper_action">
+                  <div class="image-cropper_action">
                   <span>
                     <div class="avatar-upload-select-text">
                       <span tabindex="0" class="avatar-upload-button">
@@ -67,106 +68,107 @@
                       </span>
                     </div>
                   </span>
-                </div>
-              </Modal>
+                  </div>
+                </Modal>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="setting-legacy-form-item legacy-form-item-nickname">
-          <div class="setting-form-item-label">
-            <label for="nickName">
-              <span>昵称</span>
-              <span class="form-item-required">&nbsp;*</span>
-            </label>
-          </div>
-          <div class="setting-legacy-form-item-control-wrapper">
-            <div class="nickname-editor-control">
+          <div class="setting-legacy-form-item legacy-form-item-nickname">
+            <div class="setting-form-item-label">
+              <label for="nickName">
+                <span>昵称</span>
+                <span class="form-item-required">&nbsp;*</span>
+              </label>
+            </div>
+            <div class="setting-legacy-form-item-control-wrapper">
+              <div class="nickname-editor-control">
               <span class="nickname-editor-control-content">
                 <input autocomplete="off"
                        :class="['nickName-module_input','item-border',userInfo.nickname && userInfo.nickname.length > 16 ? 'error-editor-control' : '']"
                        type="text" placeholder="必填" v-model="userInfo.nickname" id="nickName" maxlength="50">
               </span>
-            </div>
-            <div class="setting-legacy-form-explain">
-              <span v-show="userInfo.nickname && userInfo.nickname.length > 16">最多可输入 16 个字符</span>
+              </div>
+              <div class="setting-legacy-form-explain">
+                <span v-show="userInfo.nickname && userInfo.nickname.length > 16">最多可输入 16 个字符</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="setting-legacy-form-item legacy-form-item-biography">
-          <div class="setting-form-item-label">
-            <label for="biography">
-              <span>简介</span>
-              <span class="textarea-words-count">{{(userInfo.profile ? userInfo.profile.length : 0) + '/56'}}</span>
-            </label>
-          </div>
-          <div class="setting-legacy-form-item-control-wrapper">
-            <div class="biography-editor-control">
+          <div class="setting-legacy-form-item legacy-form-item-biography">
+            <div class="setting-form-item-label">
+              <label for="biography">
+                <span>简介</span>
+                <span class="textarea-words-count">{{(userInfo.profile ? userInfo.profile.length : 0) + '/56'}}</span>
+              </label>
+            </div>
+            <div class="setting-legacy-form-item-control-wrapper">
+              <div class="biography-editor-control">
               <span class="biography-editor-control-content">
                 <textarea id="biography" placeholder="简单介绍一下你自己" autocomplete="off" maxlength="200"
                           :class="['biography-module_textarea','item-border',userInfo.profile && userInfo.profile.length > 56 ? 'error-editor-control' : '']"
                           v-model="userInfo.profile">
                 </textarea>
               </span>
-            </div>
-            <div class="setting-legacy-form-explain">
-              <span v-show="userInfo.profile && userInfo.profile.length > 56">最多可输入 56 个字符</span>
+              </div>
+              <div class="setting-legacy-form-explain">
+                <span v-show="userInfo.profile && userInfo.profile.length > 56">最多可输入 56 个字符</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="setting-legacy-form-item setting-legacy-form-item-wrapper">
-          <div class="setting-legacy-form-item legacy-form-item-location">
-            <div class="setting-form-item-label">
-              <label for="location">
-                <span>地址</span>
-              </label>
-            </div>
-            <div class="setting-legacy-form-item-control-wrapper">
-              <div class="location-editor-control">
+          <div class="setting-legacy-form-item setting-legacy-form-item-wrapper">
+            <div class="setting-legacy-form-item legacy-form-item-location">
+              <div class="setting-form-item-label">
+                <label for="location">
+                  <span>地址</span>
+                </label>
+              </div>
+              <div class="setting-legacy-form-item-control-wrapper">
+                <div class="location-editor-control">
                   <span class="location-editor-control-content">
                     <input id="location" autocomplete="false" type="text" placeholder="你所在的地址" maxlength="100"
                            :class="['location-module_input','item-border',userInfo.location && userInfo.location.length > 50 ? 'error-editor-control' : '']"
                            v-model="userInfo.location">
                   </span>
-              </div>
-              <div class="setting-legacy-form-explain">
-                <span v-show="userInfo.location && userInfo.location.length > 50">最多可输入 50 个字符</span>
+                </div>
+                <div class="setting-legacy-form-explain">
+                  <span v-show="userInfo.location && userInfo.location.length > 50">最多可输入 50 个字符</span>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="setting-legacy-form-item legacy-form-item-profession">
-            <div class="setting-form-item-label">
-              <label for="profession">
+            <div class="setting-legacy-form-item legacy-form-item-profession">
+              <div class="setting-form-item-label">
+                <label for="profession">
                 <span>
                   领域
                 </span>
-              </label>
-            </div>
-            <div class="setting-legacy-form-item-control-wrapper">
-              <div class="profession-editor-control">
+                </label>
+              </div>
+              <div class="setting-legacy-form-item-control-wrapper">
+                <div class="profession-editor-control">
                 <span class="profession-editor-control-content">
                   <input id="profession" autocomplete="false" type="text" placeholder="你所在的行业" maxlength="30"
                          :class="['profession-module_input','item-border',userInfo.profession && userInfo.profession.length > 15 ? 'error-editor-control' : '']"
                          v-model="userInfo.profession">
                 </span>
-              </div>
-              <div class="setting-legacy-form-explain">
-                <span v-show="userInfo.profession && userInfo.profession.length > 15">最多可输入 15 个字符</span>
+                </div>
+                <div class="setting-legacy-form-explain">
+                  <span v-show="userInfo.profession && userInfo.profession.length > 15">最多可输入 15 个字符</span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="setting-legacy-form-item legacy-form-item-btn">
-          <div class="setting-legacy-form-item-control-wrapper">
-            <div class="update-editor-control">
+          <div class="setting-legacy-form-item legacy-form-item-btn">
+            <div class="setting-legacy-form-item-control-wrapper">
+              <div class="update-editor-control">
               <span class="update-editor-control-content">
                 <button type="button" class="update-editor-control-btn">
                   <span>更新信息</span>
                 </button>
               </span>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   </div>
 </template>
