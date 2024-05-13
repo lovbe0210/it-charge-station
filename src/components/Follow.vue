450a2fb4c5e97dcb98bdea675731b01e1ff4e89c
lovbe0210 <lovbe0210@163.com>
2024-05-13 21:47:43 +0800

    关注动态列表开发

--- src/components/Follow.vue
+++ src/components/Follow.vue
@@ -1,8 +1,8 @@
 <template>
   <b-container fluid>
     <div v-if="userInfo?.uid">
-      <b-row v-if="creatorTrend && creatorTrend.length == 0" class="creator-trend">
-        <b-card body-class="trend-item" v-for="(item,index) in creatorTrend" :key="item.id">
+      <b-row v-if="creatorTrend && creatorTrend.length > 0" class="creator-trend">
+        <!--<b-card body-class="trend-item" v-for="(item,index) in creatorTrend" :key="item.id">
           <b-card-title>
             <b-avatar variant="light" :src="item.avatar" to="some-link">
               <span v-if="!item.avatar" class="iconfont icon-all-flush" style="font-size:2.5rem;"></span>
@@ -19,7 +19,91 @@
             <span class="iconfont icon-like1" style="margin-left: 5px;"></span><span>{{item.likeCount}}</span>
           </b-card-text>
           <hr v-if="index !== creatorTrend.length-1">
-        </b-card>
+        </b-card>-->
+
+
+        <div class="trend-item" v-for="item in creatorTrend" :key="item.id">
+          <user-card :userInfo="item.userInfo" :popoverContainer="popoverContainer" class="item-left">
+            <slot>
+              <b-avatar :src="item.userInfo.avatar" class="avatar">
+                <span v-if="!item.userInfo.avatar">{{item.userInfo.username}}</span>
+              </b-avatar>
+            </slot>
+          </user-card>
+          <div class="item-right">
+            <user-card :userInfo="item.userInfo" :popoverContainer="popoverContainer">
+              <slot>
+                <span class="username">{{item.userInfo.username}}</span>
+              </slot>
+            </user-card>
+            <div class="post-content">
+              <b-link to="/ramblyJot/sadasd">
+                <p>{{item.PublicTitle}}</p>
+              </b-link>
+            </div>
+            <div class="post-image">
+              <div class="photo-content p1" v-if="item.picList?.length === 1">
+                <div>
+                  <div class="cover-url">
+                    <div class="cover-url-item">
+                      <div class="bottom-mask">
+                        <img :src="item.picList[0]" :alt="'灵感时刻-' + item.createTime" class="bottom-img">
+                      </div>
+                      <div class="blur"></div>
+                      <div class="cover-item">
+                        <img :src="item.picList[0]" :alt="'灵感时刻-' + item.createTime" class="cover-url-item">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="photo-content p2" v-if="item.picList?.length === 2">
+                <div>
+                  <div class="cover-url">
+                    <div class="cover-url-item">
+                      <div class="bottom-mask">
+                        <img v-for="pic in item.picList" :key="pic" :src="pic" :alt="'灵感时刻-' + item.createTime"
+                             class="bottom-img">
+                      </div>
+                      <div class="blur"></div>
+                      <div class="cover-item">
+                        <div v-for="pic in item.picList" :key="pic" class="cover-item-box">
+                          <img :src="pic" :alt="'灵感时刻-' + item.createTime" class="cover-url-item">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="photo-content p3" v-if="item.picList?.length === 3">
+                <div class="more-images">
+                  <div v-for="pic in item.picList" :key="pic" class="photo">
+                    <img :src="pic" :alt="'灵感时刻-' + item.createTime">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="action-box">
+              <div class="post-time">
+                <span>发布于 </span>
+                <Time class="time" :time="item.createTime" v-if="needFormatDate(item.createTime)"/>
+                <Time class="time" :time="item.createTime" v-else type="datetime"/>
+              </div>
+              <div class="action">
+              <span class="reply-btn">
+                <span class="iconfont reply"></span>
+                {{item.comments}}
+              </span>
+                <span class="like-btn">
+                <span class="iconfont like"></span>
+                {{item.likes}}
+              </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
       </b-row>
       <b-row v-else class="trend-empty">
         <span class="iconfont beauty-empty"></span>
@@ -60,36 +144,46 @@
     name: 'Follow',
     data() {
       return {
+        popoverContainer: null,
         creatorTrend: [
           {
             id: '001',
-            avatar: 'https://tvax1.sinaimg.cn/large/718153f4gy1gy47gm06y9j20m80rsq8t.jpg',
-            nickName: 'lovbe0210',
-            trendTitle: '发表了笔记 MySQL索引初见闻',
-            previewContent: 'MySQL索引初见闻MySQL索引初见闻MySQL索引初见闻MySQL索引初见闻',
-            publicTime: '10分钟前',
-            likeCount: 15,
-            commentCount: 15
+            // 动态类型 1笔记 2动态 3专栏
+            pubType: '1',
+            PublicTitle: 'MySQL索引初见闻MySQL索引初见闻MySQL索引初见闻MySQL索引初见闻',
+            PublicContent: 'MySQL索引初见闻MySQL索引初见闻MySQL索引初见闻MySQL索引初见闻',
+            pubTime: '10分钟前',
+            userInfo: {
+              userId: 1,
+              username: '闪魔亮晶晶',
+              avatar: require('@/assets/avatar/16.png')
+            }
           },
           {
             id: '002',
-            avatar: 'https://tvax3.sinaimg.cn/large/718153f4gy1gy1ob6nxj1j20lo0ot41b.jpg',
-            nickName: 'lovbe0210',
-            trendTitle: '发表了心情',
-            previewContent: '今天蒙蒙顿顿',
-            publicTime: '昨天09:30',
-            likeCount: 15,
-            commentCount: 15
+            // 动态类型 1笔记 2动态 3专栏
+            pubType: '2',
+            PublicTitle: 'MySQL索引初见闻MySQL索引初见闻MySQL索引初见闻MySQL索引初见闻',
+            PublicContent: 'MySQL索引初见闻MySQL索引初见闻MySQL索引初见闻MySQL索引初见闻',
+            pubTime: '10分钟前',
+            userInfo: {
+              userId: 1,
+              username: '闪魔亮晶晶',
+              avatar: require('@/assets/avatar/16.png')
+            }
           },
           {
             id: '003',
-            avatar: 'https://tvax1.sinaimg.cn/large/718153f4gy1gxzk3fro87j20nc0ncq3z.jpg',
-            nickName: 'lovbe0210',
-            trendTitle: '发表了专栏文章 K8S自动部署（上）—— 认识Docker',
-            previewContent: 'K8S自动部署（上）—— 认识DockerK8S自动部署（上）—— 认识DockerK8S自动部署（上）—— 认识DockerK8S自动部署（上）—— 认识DockerK8S自动部署（上）—— 认识Docker',
-            publicTime: '01-17',
-            likeCount: 15,
-            commentCount: 15
+            // 动态类型 1笔记 2动态 3专栏
+            pubType: '3',
+            PublicTitle: 'MySQL索引初见闻MySQL索引初见闻MySQL索引初见闻MySQL索引初见闻',
+            PublicContent: 'MySQL索引初见闻MySQL索引初见闻MySQL索引初见闻MySQL索引初见闻',
+            pubTime: '10分钟前',
+            userInfo: {
+              userId: 1,
+              username: '闪魔亮晶晶',
+              avatar: require('@/assets/avatar/16.png')
+            }
           }
         ]
       }
@@ -102,7 +196,10 @@
     components: {
       AuthModal
     },
-    methods: {}
+    methods: {},
+    mounted() {
+      this.popoverContainer = this.$refs.popoverContainer;
+    }
   }
 </script>
 
